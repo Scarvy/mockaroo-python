04e8f8baf54119baf63fe7ee61027c06aaadc477
Scott Carvalho <scottcarvalho71@gmail.com>
2023-10-15 18:44:04 -0400

    Add new unit tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,6 @@
 """Unit tests for the mockaroo package."""
 import pytest
+import os
 import warnings
 
 from unittest.mock import patch, Mock
@@ -18,6 +19,20 @@ def empty_client():
 def test_create():
     """Test init without passing api_key"""
     empty_client()
+
+
+def test_warning_for_no_api_key():
+    with patch.dict(os.environ, {}, clear=True), warnings.catch_warnings(
+        record=True
+    ) as w:  # Clearing all env vars
+        client = empty_client()
+        _ = client.api_key  #
+        assert len(w) == 1
+        assert issubclass(w[-1].category, UserWarning)
+        assert (
+            "API key is not provided. Set API_KEY `export API_KEY=your_api_key`."
+            == str(w[-1].message)
+        )
 
 
 def test_create_attributes():
@@ -92,7 +107,7 @@ def test_delete():
         assert result == {"success": True}
 
 
-def test_generate():
+def test_default_generate_with_schema():
     client = empty_client()
     with patch("requests.request") as mock_request:
         mock_response = Mock()
@@ -100,9 +115,37 @@ def test_generate():
         mock_response.status_code = 200
         mock_request.return_value = mock_response
 
-        result = client.generate(schema="Person", count=1, fmt="json")
+        result = client.generate(schema="Person")
 
         assert result == [{"mock_key": "mock_value"}]
+
+
+def test_generate_fmt_csv():
+    client = empty_client()
+    with patch("requests.request") as mock_request:
+        mock_response = Mock()
+        mock_response.content = b"id,col_header1,col_header2\n1,row_value1,row_value2\n"
+        mock_response.status_code = 200
+        mock_request.return_value = mock_response
+
+        result = client.generate(schema="Person", fmt="csv")
+
+        assert result == b"id,col_header1,col_header2\n1,row_value1,row_value2\n"
+
+
+def test_generate_fmt_txt():
+    client = empty_client()
+    with patch("requests.request") as mock_request:
+        mock_response = Mock()
+        mock_response.content = (
+            b"id\tcol_header1\tlcol_header2\n1\trow_value1\trow_value2\n"
+        )
+        mock_response.status_code = 200
+        mock_request.return_value = mock_response
+
+        result = client.generate(schema="Person", fmt="csv")
+
+        assert result == b"id\tcol_header1\tlcol_header2\n1\trow_value1\trow_value2\n"
 
 
 @pytest.mark.parametrize(
@@ -110,18 +153,28 @@ def test_generate():
     [
         (
             GENERATE_ENDPOINT,
-            {"count": 5, "fmt": "json"},
-            "https://api.mockaroo.com/api/generate.json?count=5&client=python&key=api_key",
+            {"schema": "Person", "fmt": "json", "count": 1},
+            "https://api.mockaroo.com/api/generate.json?schema=Person&count=1&client=python&key=api_key",
+        ),
+        (
+            GENERATE_ENDPOINT,
+            {"schema": "Person", "fmt": "csv", "count": 1},
+            "https://api.mockaroo.com/api/generate.csv?schema=Person&count=1&client=python&key=api_key",
+        ),
+        (
+            GENERATE_ENDPOINT,
+            {"schema": "Person", "fmt": "csv", "count": 5},
+            "https://api.mockaroo.com/api/generate.csv?schema=Person&count=5&client=python&key=api_key",
         ),
         (
             TYPE_ENDPOINT,
             {},
-            "https://api.mockaroo.com/api/types/?client=python&key=api_key&count=None",
+            "https://api.mockaroo.com/api/types/?client=python&key=api_key",
         ),
         (
             UPLOAD_ENDPOINT,
             {"name": "test_name"},
-            "https://api.mockaroo.com/api/datasets/test_name?client=python&key=api_key&count=None",
+            "https://api.mockaroo.com/api/datasets/test_name?client=python&key=api_key",
         ),
     ],
 )
